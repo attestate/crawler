7b0882612129f9d244557f81bba65754e428b4e0
Tim Daubenschütz <tim@daubenschuetz.de>
2023-02-03 00:05:50 +0100

    Fix crawl path

--- crawl_path.mjs
+++ crawl_path.mjs
@@ -1,31 +1,31 @@
 import { resolve } from "path";
 import { env } from "process";
-import * as blockLogs from "../crawler-call-block-logs/src/index.mjs";
+import * as blockLogs from "@attestate/crawler-call-block-logs";
 
 const range = {
   start: 16370086,
   end: 16370087, // start + 1
 };
 
+const address = "0x24da31e7bb182cb2cabfef1d88db19c2ae1f5572";
 //keccak - 256("Transfer(address,address,uint256)") == "0xddf...";
-const topic0 =
-  "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef";
-const topic1 =
-  "0x0000000000000000000000000000000000000000000000000000000000000000";
+const topics = [
+  "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef",
+];
 
 export default [
   {
     name: "call-block-logs",
     extractor: {
       module: blockLogs.extractor,
-      args: [range.start, range.end],
+      args: [range.start, range.end, address, topics],
       output: {
         path: resolve(env.DATA_DIR, "call-block-logs-extraction"),
       },
     },
     transformer: {
       module: blockLogs.transformer,
-      args: [topic0, topic1],
+      args: [],
       input: {
         path: resolve(env.DATA_DIR, "call-block-logs-extraction"),
       },
